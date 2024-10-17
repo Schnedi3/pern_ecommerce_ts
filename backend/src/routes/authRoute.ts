772708341b6c601e13f4cad953146811885b5dcd
Schnedi3 <schnedi3@gmail.com>
2024-10-17 23:27:36 +0200

    Backend / Admin: Implement google login
    * Clean api points

--- backend/src/routes/authRoute.ts
+++ backend/src/routes/authRoute.ts
@@ -3,26 +3,19 @@ import { Router } from "express";
 import { validateSchema } from "../middleware/validateAuth";
 import { registerSchema, loginSchema } from "../schemas/authSchema";
 import {
-  loginAdmin,
   loginGoogle,
-  loginUser,
-  registerUser,
+  login,
+  register,
   resetPassword,
   refreshToken,
 } from "../controllers/authController";
 
 const router = Router();
 
-// admin
-router.post("/admin", validateSchema(loginSchema), loginAdmin);
-
-// user
 router.post("/google", loginGoogle);
-router.post("/login", validateSchema(loginSchema), loginUser);
-router.post("/register", validateSchema(registerSchema), registerUser);
+router.post("/login", validateSchema(loginSchema), login);
+router.post("/register", validateSchema(registerSchema), register);
 router.put("/reset-password", validateSchema(loginSchema), resetPassword);
-
-// both
 router.post("/refresh-token", refreshToken);
 
 export default router;
