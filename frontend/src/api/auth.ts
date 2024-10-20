991014fea44b964c12825bd506eee83998ebc9fa
Schnedi3 <schnedi3@gmail.com>
2024-10-20 19:55:22 +0200

    Test: Disable refresh token api

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -75,27 +75,27 @@ export const useResetPassword = () => {
   });
 };
 
-export const generateRefreshToken = () => {
-  return axios.get("/auth/refresh-token");
-};
+// export const generateRefreshToken = () => {
+//   return axios.get("/auth/refresh-token");
+// };
 
-axios.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const originalRequest = error.config;
+// axios.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   async (error) => {
+//     const originalRequest = error.config;
 
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//       originalRequest._retry = true;
 
-      try {
-        await generateRefreshToken();
-        return axios(originalRequest);
-      } catch (refreshError) {
-        console.log(`Token refresh error: ${refreshError}`);
-      }
-    }
-    return Promise.reject(error);
-  }
-);
+//       try {
+//         await generateRefreshToken();
+//         return axios(originalRequest);
+//       } catch (refreshError) {
+//         console.log(`Token refresh error: ${refreshError}`);
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
